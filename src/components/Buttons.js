1fc6790ddebf8feb7f23a022db64fd712ab339c0
gianluca galota <gianluca.galota@gmail.com>
2022-03-21 12:11:43 +0100

    utils defaultTheme created

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,32 +1,33 @@
-import styled from 'styled-components'
-
-const primaryColor = '#F78F28';
-const warningColor = '#D0454C';
+import styled from 'styled-components';
+import { defaultTheme } from '../utils';
+import { typeScale } from '../utils';
+import { colors, red, neutral } from '../utils';
 
 
 const Button = styled.button`
   padding: 12px 24px;
-  font-size: 1rem;
+  font-size: ${typeScale.paragraph};
   min-width: 100px;
   border-radius: 6px;
   cursor: pointer;
+  font-family: ${typeScale.primaryFont};
 `
 
 const PrimaryButton = styled(Button)`
-  background-color: ${primaryColor};
+  background-color: ${defaultTheme.primaryColor};
   border: none;
-  color: white;
+  color: ${neutral[100]};
 `;
 
 const SecondaryButton = styled(Button)`
-  border: 2px solid ${primaryColor};
+  border: 2px solid ${defaultTheme.primaryColor};
   background: none;
-  color: ${primaryColor};
+  color: ${defaultTheme.primaryColor};
 `
 
 const WarningButton = styled(Button)`
-  background-color: ${warningColor};
-  color: white;
+  background-color: ${red[100]};
+  color: ${neutral[100]};
   border: none;
 
 `
---
+++ src/utils/themes.js
@@ -0,0 +1,12 @@
+import { orange, neutral } from './colors';
+import { primaryFont } from './typography';
+
+export const defaultTheme = {
+  primaryColor: orange[300],
+  primaryHoverColor: orange[400],
+  primaryActiveColor: orange[500],
+  textColorOnPrimary: neutral[100],
+  textColor: neutral[600],
+  textColorInverted: neutral[100],
+  primaryFont: primaryFont
+}
